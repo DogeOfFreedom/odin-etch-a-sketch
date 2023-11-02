9948331d005c55b2c998d748e73cba642054bb1f
DogeOfFreedom <jchensi2000@gmail.com>
2023-11-02 18:53:58 +0800

    feat: add prompt asking for canvas size

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let changeCanvasSize = () => {
+    let canvasSize = prompt("Enter Dimensions (max 100)");
+    while(isNaN(canvasSize) || canvasSize > 100) {
+        canvasSize = prompt("Invalid dimensions, Enter Dimensions (max 100)");
+    }
+}
+
 const cells = Array.from(document.querySelectorAll(".grid-cell"));
 cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {
